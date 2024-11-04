56f63db93fdeb858d0ce37ad3b99c712eb269d1d
Prachi Anand <prachianand321@gmail.com>
2024-11-04 13:47:41 +0530

    Thursday usercontroller.js

---
+++ server/controllers/userController.js
@@ -0,0 +1,34 @@
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
+require("dotenv").config();
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password, phoneNumber } = req.body;
+
+    if (!name || !email || !password || !phoneNumber) {
+        res.status(400);
+        throw new Error("Please provide all fields");
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await user.create({
+        name,
+        email,
+        phoneNumber,
+        password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "user registered successfully", user });
+});
+
+module.exports = {
+    registerUser,
+};

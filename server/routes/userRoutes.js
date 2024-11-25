a1a60dd91b2f388da0da2d5a5e611aef0450031b
Prachi Anand <prachianand321@gmail.com>
2024-11-25 10:46:44 +0530

    jwt error resolved in userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const router=express.Router();
-const { jwtAuthMiddleware } = require("../middleware/jwtAuthMiddleware");
+const { validateJwtToken } = require("../middleware/jwtAuthMiddleware");
 
 const{
     registerUser,
@@ -8,18 +8,18 @@ const{
     getUserProfile,
     updateUserProfile,
 }=require("../controllers/userController");
+// console.log(registerUser,loginUser,getUserProfile,updateUserProfile);
 
 router.post("/register", registerUser);
 
 //route for get the user specific data
-// router.get("/myaccount", jwtAuthMiddleware,getUserProfile);
-router.get("/myaccount", getUserProfile);
+router.get("/myaccount", validateJwtToken,getUserProfile);
+
 
 //route fir updating the user specific data
-// router.patch("/myaccount",jwtAuthMiddleware,updateUserProfile);
-router.patch("/myaccount", updateUserProfile);
+router.patch("/myaccount",validateJwtToken, updateUserProfile);
 
 //route for user login
-router.post("/login",jwtAuthMiddleware,loginUser);
+router.post("/login",loginUser);
 
 module.exports=router;

5f9a7251040c3f5fe596a771990f5bfa58ec7cfe
Prachi Anand <prachianand321@gmail.com>
2024-11-25 10:45:15 +0530

    modified errorHandler.js

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -3,27 +3,26 @@ const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     switch (statusCode) {
         case constants.VALIDATION_ERROR:
-            res.json({
+            return res.status(statusCode).json({
                 title: "Validation Failed",
                 message: err.message,
                 stackTrace: err.stack
             });
 
-            break;
         case constants.NOT_FOUND:
-            res.json({
+            return res.status(statusCode).json({
                 title: "Not found",
                 message: err.message,
                 stackTrace: err.stack
             });
         case constants.UNAUTHORIZED:
-            res.json({
+            return res.status(statusCode).json({
                 title: "Unauthorized",
                 message: err.message,
                 stackTrace: err.stack
             });
         case constants.SERVER_ERROR:
-            res.json({
+            return res.status(statusCode).json({
                 title: "Server Error",
                 message: err.message,
                 stackTrace: err.stack
@@ -31,7 +30,11 @@ const errorHandler = (err, req, res, next) => {
 
         default:
             console.log("No error, all good!");
-            break;
+            return res.status(500).json({
+                title: "Internal Server Error",
+                message: "Something went wrong, Please try agan later...",
+                stackTrace: err.stack
+            })
     }
 };
 module.exports = errorHandler;
